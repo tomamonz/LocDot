c1ea6da15b09c9929f43982025647c1558f0670f
tomamonz <zaleskit6@gmail.com>
2023-12-06 21:22:57 +0100

    Initial commit

---
+++ back/locDot/src/main/java/com/gt/locDot/LocDotApplication.java
@@ -0,0 +1,13 @@
+package com.gt.locDot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocDotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocDotApplication.class, args);
+	}
+
+}
